85f5558d20284643d18ba1e27a2e9614be762359
Maksym Pecherytsia <findimrk@gmail.com>
2022-11-20 22:33:31 +0200

    add API SLice RTK Query

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import heroes from "../components/heroesList/heroesSlice";
 import filters from "../components/heroesFilters/filtersSlice";
+import { apiSlice } from "../api/apiSlice";
 
 const stringMiddleware = () => (next) => (action) => {
 	if (typeof action === "string") {
@@ -37,9 +38,9 @@ const stringMiddleware = () => (next) => (action) => {
 // );
 
 const store = configureStore({
-	reducer: { heroes, filters },
+	reducer: { heroes, filters, [apiSlice.reducerPath]: apiSlice.reducer },
 	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(stringMiddleware),
+		getDefaultMiddleware().concat(stringMiddleware, apiSlice.middleware),
 	devTools: process.env.NODE_ENV !== "production",
 });
 
